e3780c7d49c00afdf40af1a4f5f8a3abd5445ff1
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2021-07-12 23:09:45 -0500

    reduced damage maybe? pog? no

--- CustomRoles/Roles/DwarfZombie.cs
+++ CustomRoles/Roles/DwarfZombie.cs
@@ -1,4 +1,6 @@
 using CustomRoles.API;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
 using MEC;
 using UnityEngine;
 
@@ -15,5 +17,27 @@ namespace CustomRoles.Roles
         protected override void RoleAdded() => Timing.CallDelayed(2.5f, () => Player.Scale = new Vector3(0.75f, 0.75f, 0.75f));
 
         protected override void RoleRemoved() => Player.Scale = Vector3.one;
+
+        protected override void LoadEvents()
+        {
+            Log.Debug($"{Name} loading events.");
+            Exiled.Events.Handlers.Player.Hurting += OnHurt;
+        }
+
+        protected override void UnloadEvents()
+        {
+            Log.Debug($"{Name} unloading events.");
+            Exiled.Events.Handlers.Player.Hurting -= OnHurt;
+        }
+
+        private void OnHurt(HurtingEventArgs ev)
+        {
+            if (ev.Attacker == Player)
+            {
+                var dmgCal = ev.Amount * 0.3;
+                var dmgPer = ev.Amount - dmgCal;
+                ev.Amount = (float) dmgPer;
+            }
+        }
     }
 }
